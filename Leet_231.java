a7f07b2f16e6f2253b1720cb45b4ca2ed0ac5f83
EZDevanshu <devanshuparmar00@gmail.com>
2026-01-01 22:56:34 +0530

    login modifed

--- Leet_231.java
+++ Leet_231.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 class Solution {
 public
     boolean isPowerOfTwo(int n) {
-        if(n<=0){
-        	return false;
-        }
-        return  (n & (n -1)) == 0; 
+
+        if(n>=0 && (n & (n - 1)) == 0){
+			return true;
+		} 
+		else{
+			return false;
+		}
     }
 }
 public class Leet_231{
