ac65fbe398e4aba7f342e3f58b73c0ad1a6a4562
EZDevanshu <devanshuparmar00@gmail.com>
2026-02-16 18:46:27 +0530

    feat: implement addTwoNumbers method to sum two linked lists representing numbers

--- Leet_2.java
+++ Leet_2.java
@@ -29,3 +29,31 @@
 // public class Leet_2 {
     
 // }
+
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dmy = new ListNode(0);
+        ListNode tail = dmy;
+
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0){
+            int v1 = (l1 != null) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+
+            int sum = v1 + v2 + carry;
+
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            tail.next = new ListNode(digit);
+            tail = tail.next;
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }    
+
+        return dmy.next;    
+    }
+}
