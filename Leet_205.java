67648c535515843878d9fb05f19234e4bd6294f4
EZDevanshu <devanshuparmar00@gmail.com>
2026-02-20 18:40:07 +0530

    feat: implement isIsomorphic method in Leet_205 class and add main method for testing; remove Q1 class and add project configuration files

--- Leet_205.java
+++ Leet_205.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character , Integer> mp1 = new HashMap<>();
+        HashMap<Character , Integer> mp2 = new HashMap<>();
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(!mp1.containsKey(s.charAt(i))){
+                mp1.put(s.charAt(i) , i);
+            }
+            if(!mp2.containsKey(t.charAt(i))){
+                mp2.put(t.charAt(i) , i);
+            }
+            if(!mp1.get(s.charAt(i)).equals(mp2.get(t.charAt(i)))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
 public class Leet_205 {
+    public static void main(String[] args) {
+        String s = "f11";
+        String t = "b23";
+
+        Solution obj = new Solution();
+        boolean ans = obj.isIsomorphic(s , t);
+
+        System.out.println(ans);
+    }
 }
