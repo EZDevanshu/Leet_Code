5b5b3b288bc3934e979d3c49e6bf2163d5275a20
EZDevanshu <devanshuparmar00@gmail.com>
2026-02-21 18:55:58 +0530

    feat: add Leet_383 class with solution for constructing ransom note from magazine

---
+++ Leet_383.java
@@ -0,0 +1,31 @@
+
+class Solution {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr1 = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            char ch = magazine.charAt(i);
+            arr1[ch - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            if(arr1[ch - 'a'] == 0){
+                return false;
+            }
+            arr1[ch - 'a']--;
+        }
+        return true;
+    }
+}
+
+public class Leet_383 {
+
+    public static void main(String[] args) {
+        String s = "aa";
+        String b = "aab";
+
+        Solution obj = new Solution();
+        boolean ans = obj.canConstruct(s, b);
+    }
+}
