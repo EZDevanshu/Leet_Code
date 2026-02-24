45d915a553621d6bef0d64ab1b801c3679d76e97
EZDevanshu <devanshuparmar00@gmail.com>
2026-02-24 21:25:19 +0530

    feat: add Leet_763 class with solution for partition labels problem

--- Leet_763.java
+++ Leet_763.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        HashMap<Character , Integer> map = new HashMap<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            map.put(ch , i);
+        }
+
+        int max = 0;
+        int pre = -1;
+
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            max = Math.max(max , map.get(ch));
+            if(max == i){
+                ans.add(max - pre);
+                pre = max;
+            }
+        }
+        return ans;
+    }
+}
 
 public class Leet_763 {
+    public static void main(String[] args) {
+        String s = "ababcbacadefegdehijhklij";
+
+        Solution obj = new Solution();
+        System.out.println((obj.partitionLabels(s)));
+    }
 }
