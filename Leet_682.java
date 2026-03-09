6123800c21f34a690e49acf8f79df2e13bcaf3d1
EZDevanshu <devanshuparmar00@gmail.com>
2026-03-09 19:22:14 +0530

    feat: implement Leet_682 class for baseball game score calculation

--- Leet_682.java
+++ Leet_682.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
-public class Leet_682 {
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0 ; i < operations.length; i++){
+            if(operations[i].equals("+")){
+                int t1 = st.peek();
+                st.pop();
+                int t2 = st.peek();
+                st.push(t1);
+                st.push(t1 + t2);
+            }
+            else if (operations[i].equals("D")){
+                st.push(st.peek() * 2);
+            }
+            else if(operations[i].equals("C")){
+                st.pop();
+            }
+            else{
+                st.push(Integer.parseInt(operations[i]));
+            }
+        }
+
+        int sum = 0 ;
+
+        while(!st.isEmpty()){
+            sum += st.peek();
+            st.pop();
+        }
+        return sum;
+    }
 }
