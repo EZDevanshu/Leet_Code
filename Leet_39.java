f2b528f55721d2205523e8a079a974e330953ef4
EZDevanshu <devanshuparmar00@gmail.com>
2026-04-10 23:29:58 +0530

    feat: add Solution class with implementation for combination sum problem

---
+++ Leet_39.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        HashMap<Integer , Integer> mp = new Map<>();
+
+        for(int x : candidates){
+                
+        }   
+    }
+}
+
+class Leet_39{
+    public static void main(String[] args) {
+        int arr[] = {2,3,6,7};
+        int target = 7;
+
+        Solution obj = new Solution();
+        System.out.println(obj.combinationSum(arr , target));
+    }
+}
