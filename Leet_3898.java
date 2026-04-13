57c5418261258c5430a1afcc2af3375a9bebbaa5
EZDevanshu <devanshuparmar00@gmail.com>
2026-04-13 18:22:48 +0530

    Leetcode

---
+++ Leet_3898.java
@@ -0,0 +1,10 @@
+class Solution:
+    def findDegrees(self, matrix: list[list[int]]) -> list[int]:
+        li = []
+        for i in matrix :
+            count = 0
+            for j in i :
+                if j == 1:
+                    count += 1
+            li.append(count)
+        return li
