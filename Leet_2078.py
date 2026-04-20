d3b70dd3d0ea97cddb5e3fb298ad3403572c79b3
EZDevanshu <devanshuparmar00@gmail.com>
2026-04-20 18:28:51 +0530

    optimize approch

--- Leet_2078.py
+++ Leet_2078.py
@@ -1,9 +1,28 @@
-
+# Optimize approch
+ 
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
-        maxVal = 0
-        for i in range(len(colors)) :
-            for j in range(i + 1 , len(colors)):
-                if colors[i] != colors[j]:
-                    maxVal = max(maxVal , abs(j - i))
-        return maxVal 
+        maxDist = 0
+
+        for i in range(len(colors)):
+                
+            if colors[i] != colors[0] :
+                maxDist = max(maxDist , i)
+
+            if colors[i] != colors[len(colors) - 1]:
+                maxDist = max(maxDist , len(colors) - 1 - i)
+
+        return maxDist
+    
+
+#  brute force approach 
+
+
+# class Solution:
+#     def maxDistance(self, colors: List[int]) -> int:
+#         maxVal = 0
+#         for i in range(len(colors)) :
+#             for j in range(i + 1 , len(colors)):
+#                 if colors[i] != colors[j]:
+#                     maxVal = max(maxVal , abs(j - i))
+#         return maxVal     
