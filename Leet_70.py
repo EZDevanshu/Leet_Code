bffe9a052e65487bc4586dac8f13d51b64cde2b1
EZDevanshu <devanshuparmar00@gmail.com>
2026-05-01 16:48:54 +0530

    feat: implement climbStairs method to calculate the number of ways to climb stairs

---
+++ Leet_70.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [-1] * (n + 1)
+
+        def rec(n) :
+            if n <= 2:
+                return n 
+            if dp[n] != -1 :
+                return dp[n] 
+            
+            dp[n] = rec(n - 1) + rec(n - 2) 
+            return dp[n]  
+        return rec(n)
